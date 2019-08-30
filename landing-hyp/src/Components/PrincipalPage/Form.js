b9a77cffabc664b7b770ec7a8779618af6a9c956
GiovannaGlz <priscila_glz_27@hotmail.es>
2019-08-30 09:19:13 -0500

    groupform, checkbox

--- landing-hyp/src/Components/PrincipalPage/Form.js
+++ landing-hyp/src/Components/PrincipalPage/Form.js
@@ -1,6 +1,9 @@
 import React, {Component} from 'react';
 import TextField from '@material-ui/core/TextField';
 import { FormControl, MenuItem } from '@material-ui/core';
+import Checkbox from '@material-ui/core/Checkbox';
+import FormGroup from '@material-ui/core/FormGroup';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
 
 const currencies = [
     {
@@ -25,62 +28,77 @@ class Form extends Component{
     render(){
           
         return(
-            <FormControl>
+            
+            <FormGroup>
+                    <FormControl>
+                    
+                        <TextField
+                            id="filled-name-input"
+                            label="Nombre Completo"
+                            className="Name"
+                            type="name"
+                            name="name"
+                            autoComplete="name"
+                            margin="normal"
+                            variant="filled"
+                        />
+
+                        <TextField required
+                            id="filled-email-input"
+                            label="Email"
+                            className="Email"
+                            type="email"
+                            name="email"
+                            autoComplete="email"
+                            margin="normal"
+                            variant="filled"
+                        />
+
+                        <TextField required
+                            id="filled-password-input"
+                            label="Contraseña"
+                            className="Password"
+                            type="password"
+                            autoComplete="current-password"
+                            margin="normal"
+                            variant="filled"
+                        />
+
+                        <TextField
+                            id="filled-number"
+                            label="Teléfono"
+                            value=""
+                            margin="normal"
+                            variant="filled"
+                        />
+
+                        <TextField
+                            id="filled-select-currency"
+                            select
+                            label="Select"
+                            className="Select"
+                            value={currencies.value}
+                            helperText="Please select your currency"
+                            margin="normal"
+                            variant="filled"
+                        />
+
+                         <FormControlLabel
+                            control={
+                            <Checkbox
+                                checked="true"
+                                value="checked"
+                                color="primary"
+                            />
+                            }
+                            label="Acepto términos y condiciones en infiniti.mx"
+                        />
+
+                    </FormControl>
+                </FormGroup>
                 
-                <TextField
-                    id="filled-name-input"
-                    label="Nombre Completo"
-                    className="Name"
-                    type="name"
-                    name="name"
-                    autoComplete="name"
-                    margin="normal"
-                    variant="filled"
-                />
-
-                <TextField required
-                    id="filled-email-input"
-                    label="Email"
-                    className="Email"
-                    type="email"
-                    name="email"
-                    autoComplete="email"
-                    margin="normal"
-                    variant="filled"
-                />
-
-                <TextField required
-                    id="filled-password-input"
-                    label="Contraseña"
-                    className="Password"
-                    type="password"
-                    autoComplete="current-password"
-                    margin="normal"
-                    variant="filled"
-                />
-
-                <TextField
-                    id="filled-number"
-                    label="Teléfono"
-                    value=""
-                    margin="normal"
-                    variant="filled"
-                />
-
-                <TextField
-                    id="filled-select-currency"
-                    select
-                    label="Select"
-                    className="Select"
-                    value={currencies.value}
-                    helperText="Please select your currency"
-                    margin="normal"
-                    variant="filled"
-                    />
-
-            </FormControl>
-        )
+            )
+        }
     }
-}
 
 export default Form;
