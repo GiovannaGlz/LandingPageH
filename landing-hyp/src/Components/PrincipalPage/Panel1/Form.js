616e5d76139099007f25aaa68ac3e257d2d4aaf9
GiovannaGlz <priscila_glz_27@hotmail.es>
2019-08-31 01:41:31 -0500

    Evento handleSubmit en formulario

--- landing-hyp/src/Components/PrincipalPage/Panel1/Form.js
+++ landing-hyp/src/Components/PrincipalPage/Panel1/Form.js
@@ -1,49 +1,115 @@
-import React, {Component} from 'react';
-import Typography from '@material-ui/core/Typography';
+import React, { Component } from 'react';
+import {Typography, TextField, MenuItem} from '@material-ui/core';
+
+const agency = [
+    {
+        country: 'Polanco',
+    },
+    {
+        country: 'Pedregal',
+    },
+    {
+        country: 'Toluca',
+    },
+    {
+        country: 'Satélite',
+    },
+  ];
 
 class Form extends Component {
     constructor(props) {
-      super(props);
-      this.state = {
-        value: '',
-        isLoggedIn: false,
-    };
-  
-      this.handleChange = this.handleChange.bind(this);
-      this.handleSubmit = this.handleSubmit.bind(this);
+        super(props);
+        this.state = {
+            name: '',
+            email: '',
+            password: '',
+            agency: 'Polanco',
+            isSubmit: false,
+        };
+
+        //this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
     }
-  
-    handleChange(event) {
-      this.setState({value: event.target.value});
-    }
-  
+
+    // handleChange(event) {
+    //     this.setState({
+    //         name: event.target.value,
+    //         email: event.target.value,
+    //         password: event.target.value,
+    //         agency: event.target.value,
+    //     });
+    // }
+
     handleSubmit(event) {
-        this.setState({isLoggedIn: true});
+        this.setState({ isSubmit: true });
     }
-  
+
     render() {
-        const isLoggedIn = this.state.isLoggedIn;
+        const isSubmit = this.state.isSubmit;
         let view;
 
-        if (isLoggedIn) {
-           view = <Typography variant="body1" gutterBottom>
-                    Gracias por registrarte, en breve te contactaremos
+        if (isSubmit) {
+            view = <Typography variant="body1" gutterBottom>
+                Gracias por registrarte, en breve te contactaremos
                  </Typography>
-          } else {
+        } else {
             view = <form onSubmit={this.handleSubmit}>
-                        <label>
-                            Name:
-                            <input type="text" value={this.state.value} onChange={this.handleChange} />
-                            </label>
-                            <input type="submit" value="Submit" onClick={this.handleSubmit}/>
-                    </form>;
-          }
+                <TextField
+                        id="outlined-name"
+                        label="Nombre"
+                        type="text"
+                        value={this.state.name}
+                       // onChange={this.handleChange(this.setState.name)}
+                        autoComplete="current-name"
+                        margin="normal"
+                    />
 
-    return (
-        <div isLoggedIn={isLoggedIn}>
-            {view}
-            </div>
-            );
-            }
+                <TextField
+                        id="outlined-email-input"
+                        label="Email"
+                        type="email"
+                        value={this.state.email}
+                        //onChange={this.handleChange(this.setState.email)}
+                        autoComplete="current-email"
+                        margin="normal"
+                    />
+
+                <TextField
+                        id="outlined-password-input"
+                        label="Password"
+                        type="password"
+                        value={this.state.password}
+                        //onChange={this.handleChange(this.setState.password)}
+                        autoComplete="current-password"
+                        margin="normal"
+                    />
+
+                <TextField
+                        id="outlined-select-currency"
+                        select
+                        label="Agencia"
+                        value={this.state.agency}
+                        //onChange={this.handleChange(this.setState.agency)}
+                        helperText="Por favor selecciona una agencia"
+                        margin="normal"
+                        variant="outlined"
+                    >
+                        {agency.map(option => (
+                        <MenuItem key={option.country} value={option.country}>
+                            {option.country}
+                        </MenuItem>
+                        ))}
+                    </TextField>
+
+                <input type="submit" value="Submit" onClick={this.handleSubmit} />
+            </form>;
         }
-    export default Form;
+
+        return (
+            <div isSubmit={isSubmit}>
+                {view}
+            </div>
+        );
+    }
+}
+export default Form;
